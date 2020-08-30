11c39e93a51b65a0c6c8c39722f4b60d01b91f95
mrkhmath <46818606+mrkhmath@users.noreply.github.com>
2020-08-30 16:49:36 +0400

    Add files via upload

---
+++ build/precache-manifest.22f9eb9a2e78ab7ac4c2f9ea25fbfe30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee93489e14080e08817c2a7d79267d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d12fc88c91bd7abe1b23",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56b0193fbfcc1345eb7e",
+    "url": "/static/js/2.0de04dc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0de04dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12fc88c91bd7abe1b23",
+    "url": "/static/js/main.9681445a.chunk.js"
+  },
+  {
+    "revision": "c8a46c3baebab83d06bc",
+    "url": "/static/js/runtime-main.b625c9dd.js"
+  }
+]);
